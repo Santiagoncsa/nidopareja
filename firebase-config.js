8281bdf0f2371160bcf6f53dc26ba9b00c49167e
Santi912 <santiagoncsa@gmail.com>
2026-09-21 09:32:18 -0300

    Actualización con versión final de mi proyecto

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// -----------------------------------------------------------------
+// PASO IMPORTANTE: pegá acá la configuración que te da Firebase.
+// La encontrás en: Configuración del proyecto > Tus apps > (ícono </>) > Config
+// Reemplazá cada "PEGAR_ACA" por el valor real. No borres las comillas.
+// -----------------------------------------------------------------
+const firebaseConfig = {
+  apiKey: "AIzaSyDONqGmsMtsTem5z6lkrbI04wJyOeGzIBU",
+  authDomain: "nido-c8615.firebaseapp.com",
+  databaseURL: "https://nido-c8615-default-rtdb.firebaseio.com/",
+  projectId: "nido-c8615",
+  storageBucket: "nido-c8615.firebasestorage.app",
+  messagingSenderId: "246014186932",
+  appId: "1:246014186932:web:5465a4d9f94bfba05d8ee1",
+};
